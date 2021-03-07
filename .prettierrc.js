f367871bc001915a86e3f828c44fafc32c667045
Minhyeong Jang <react@styleshare.kr>
2021-03-08 00:14:00 +0900

    feat: config 변경

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  ...require('@grafana/toolkit/src/config/prettier.plugin.config.json'),  printWidth: 80,
+  ...require('@grafana/toolkit/src/config/prettier.plugin.config.json'),
+  printWidth: 80,
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   trailingComma: 'all',
   quoteProps: 'consistent',
   useTabs: false,
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
 };
